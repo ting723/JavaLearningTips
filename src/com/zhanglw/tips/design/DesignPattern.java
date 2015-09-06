a6a96d602ed66eacdd8726f3ed43d9dad19ea4c5
ting723@163.com <ting723@163.com>
2015-09-06 16:05:13 +0800

    添加设计模式Demo

---
+++ src/com/zhanglw/tips/design/DesignPattern.java
@@ -0,0 +1,13 @@
+package com.zhanglw.tips.design;
+
+import com.zhanglw.tips.base.TestTips;
+
+public class DesignPattern implements TestTips{
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
